7f663d4ed0146fac19b991998498678ebb7b5071
soyoonJ <sytkfkd96@gmail.com>
2024-11-28 02:25:40 +0900

    fix: ScheduleTable.tsx useDndContext -> useDndMonitor로 수정 - useDndMonitor: 컴포넌트의 리렌더링 없이 이벤트 콜백을 처리

--- src/basic/ScheduleTable.tsx
+++ src/basic/ScheduleTable.tsx
@@ -2,11 +2,11 @@ import { Box, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 import { CellSize, DAY_LABELS, 분 } from "./constants.ts";
 import { Schedule } from "./types.ts";
 import { parseHnM } from "./utils.ts";
-import { useDndContext } from "@dnd-kit/core";
-import { useMemo } from "react";
+import { memo, useCallback, useState } from "react";
 import DayItem from "../components/scheduleTable/DayItem.tsx";
 import TimeRow from "../components/scheduleTable/TimeRow.tsx";
 import DraggableSchedule from "../components/scheduleTable/DraggableSchedule.tsx";
+import { useDndMonitor } from "@dnd-kit/core";
 
 interface Props {
 	tableId: string;
@@ -27,74 +27,84 @@ const TIMES = [
 		.map((v) => `${parseHnM(v)}~${parseHnM(v + 50 * 분)}`),
 ] as const;
 
-const ScheduleTable = ({ tableId, schedules, onScheduleTimeClick, onDeleteButtonClick }: Props) => {
-	const getColor = (lectureId: string): string => {
-		const lectures = [...new Set(schedules.map(({ lecture }) => lecture.id))];
-		const colors = ["#fdd", "#ffd", "#dff", "#ddf", "#fdf", "#dfd"];
-		return colors[lectures.indexOf(lectureId) % colors.length];
-	};
+const ScheduleTable = memo(
+	({ tableId, schedules, onScheduleTimeClick, onDeleteButtonClick }: Props) => {
+		const getColor = useCallback(
+			(lectureId: string): string => {
+				const lectures = [...new Set(schedules.map(({ lecture }) => lecture.id))];
+				const colors = ["#fdd", "#ffd", "#dff", "#ddf", "#fdf", "#dfd"];
+				return colors[lectures.indexOf(lectureId) % colors.length];
+			},
+			[schedules]
+		);
 
-	const dndContext = useDndContext();
+		const [activeTableId, setActiveTableId] = useState<string | null>(null);
 
-	const getActiveTableId = () => {
-		const activeId = dndContext.active?.id;
-		if (activeId) {
-			return String(activeId).split(":")[0];
-		}
-		return null;
-	};
+		useDndMonitor({
+			onDragStart(event) {
+				const activeId = event.active.id;
+				if (activeId) {
+					setActiveTableId(String(activeId).split(":")[0]);
+				}
+			},
+			onDragEnd() {
+				setActiveTableId(null);
+			},
+			onDragCancel() {
+				setActiveTableId(null);
+			},
+		});
 
-	const activeTableId = useMemo(getActiveTableId, [dndContext.active?.id]);
-
-	return (
-		<Box
-			position="relative"
-			outline={activeTableId === tableId ? "5px dashed" : undefined}
-			outlineColor="blue.300"
-		>
-			<Grid
-				templateColumns={`120px repeat(${DAY_LABELS.length}, ${CellSize.WIDTH}px)`}
-				templateRows={`40px repeat(${TIMES.length}, ${CellSize.HEIGHT}px)`}
-				bg="white"
-				fontSize="sm"
-				textAlign="center"
-				outline="1px solid"
-				outlineColor="gray.300"
+		return (
+			<Box
+				position="relative"
+				outline={activeTableId === tableId ? "5px dashed" : undefined}
+				outlineColor="blue.300"
 			>
-				<GridItem key="교시" borderColor="gray.300" bg="gray.100">
-					<Flex justifyContent="center" alignItems="center" h="full" w="full">
-						<Text fontWeight="bold">교시</Text>
-					</Flex>
-				</GridItem>
-				{DAY_LABELS.map((day) => (
-					<DayItem key={day} day={day} />
-				))}
-				{TIMES.map((time, timeIndex) => (
-					<TimeRow
-						key={`시간-${timeIndex + 1}`}
-						time={time}
-						timeIndex={timeIndex}
-						onScheduleTimeClick={onScheduleTimeClick}
+				<Grid
+					templateColumns={`120px repeat(${DAY_LABELS.length}, ${CellSize.WIDTH}px)`}
+					templateRows={`40px repeat(${TIMES.length}, ${CellSize.HEIGHT}px)`}
+					bg="white"
+					fontSize="sm"
+					textAlign="center"
+					outline="1px solid"
+					outlineColor="gray.300"
+				>
+					<GridItem key="교시" borderColor="gray.300" bg="gray.100">
+						<Flex justifyContent="center" alignItems="center" h="full" w="full">
+							<Text fontWeight="bold">교시</Text>
+						</Flex>
+					</GridItem>
+					{DAY_LABELS.map((day) => (
+						<DayItem key={day} day={day} />
+					))}
+					{TIMES.map((time, timeIndex) => (
+						<TimeRow
+							key={`시간-${timeIndex + 1}`}
+							time={time}
+							timeIndex={timeIndex}
+							onScheduleTimeClick={onScheduleTimeClick}
+						/>
+					))}
+				</Grid>
+
+				{schedules.map((schedule, index) => (
+					<DraggableSchedule
+						key={`${schedule.lecture.title}-${index}`}
+						id={`${tableId}:${index}`}
+						data={schedule}
+						bg={getColor(schedule.lecture.id)}
+						onDeleteButtonClick={() =>
+							onDeleteButtonClick?.({
+								day: schedule.day,
+								time: schedule.range[0],
+							})
+						}
 					/>
 				))}
-			</Grid>
-
-			{schedules.map((schedule, index) => (
-				<DraggableSchedule
-					key={`${schedule.lecture.title}-${index}`}
-					id={`${tableId}:${index}`}
-					data={schedule}
-					bg={getColor(schedule.lecture.id)}
-					onDeleteButtonClick={() =>
-						onDeleteButtonClick?.({
-							day: schedule.day,
-							time: schedule.range[0],
-						})
-					}
-				/>
-			))}
-		</Box>
-	);
-};
+			</Box>
+		);
+	}
+);
 
 export default ScheduleTable;
