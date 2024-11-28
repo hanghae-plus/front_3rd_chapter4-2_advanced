77947ab7289ca00c0463b06b8a96d685a132a10d
Hwang Tae Young <hty0525@naver.com>
2024-11-29 00:51:31 +0900

    filteredLectures useMemo 사용 및 필터 한번에

--- src/basic/SearchDialog/index.tsx
+++ src/basic/SearchDialog/index.tsx
@@ -133,46 +133,29 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
 		majors: [],
 	});
 
-	const getFilteredLectures = () => {
+	const filteredLectures = useMemo(() => {
 		const { query = "", credits, grades, days, times, majors } = searchOptions;
-		return lectures
-			.filter(
-				(lecture) =>
-					lecture.title.toLowerCase().includes(query.toLowerCase()) ||
-					lecture.id.toLowerCase().includes(query.toLowerCase())
-			)
-			.filter(
-				(lecture) => grades.length === 0 || grades.includes(lecture.grade)
-			)
-			.filter(
-				(lecture) => majors.length === 0 || majors.includes(lecture.major)
-			)
-			.filter(
-				(lecture) => !credits || lecture.credits.startsWith(String(credits))
-			)
-			.filter((lecture) => {
-				if (days.length === 0) {
-					return true;
-				}
-				const schedules = lecture.schedule
-					? parseSchedule(lecture.schedule)
-					: [];
-				return schedules.some((s) => days.includes(s.day));
-			})
-			.filter((lecture) => {
-				if (times.length === 0) {
-					return true;
-				}
-				const schedules = lecture.schedule
-					? parseSchedule(lecture.schedule)
-					: [];
-				return schedules.some((s) =>
-					s.range.some((time) => times.includes(time))
-				);
-			});
-	};
 
-	const filteredLectures = getFilteredLectures();
+		return lectures.filter(
+				(lecture) =>
+				(lecture.title.toLowerCase().includes(query.toLowerCase()) ||
+					lecture.id.toLowerCase().includes(query.toLowerCase())) &&
+				(grades.length === 0 || grades.includes(lecture.grade)) &&
+				(majors.length === 0 || majors.includes(lecture.major)) &&
+				(!credits || lecture.credits.startsWith(String(credits))) &&
+				(days.length === 0 ||
+					(lecture.schedule &&
+						parseSchedule(lecture.schedule).some((s) =>
+							days.includes(s.day)
+						))) &&
+				(times.length === 0 ||
+					(lecture.schedule &&
+						parseSchedule(lecture.schedule).some((s) =>
+					s.range.some((time) => times.includes(time))
+						)))
+				);
+	}, [lectures, searchOptions]);
+
 	const lastPage = Math.ceil(filteredLectures.length / PAGE_SIZE);
 	const visibleLectures = filteredLectures.slice(0, page * PAGE_SIZE);
 	const allMajors = [...new Set(lectures.map((lecture) => lecture.major))];
