ed47e59e2164ba8106a65757f7fd7cc74f47b86d
kyh196201 <kyh196201@gmail.com>
2024-11-28 19:10:02 +0900

    refactor: ScheduleTable 컴포넌트 마다 DndContext 사용하도록 수정
    - 모든 ScheduleTable 컴포넌트가 하나의 DndContext를 사용하고 있어 하나의
      테이블에서 드래그 시 모든 테이블에 리렌더링되는 이슈가 발생했습니다.
      각 ScheduleTable 컴포넌트 마다 DndContext를 사용해서 드래그하고 있는
      테이블만 렌더링될 수 있도록 수정했습니다.

--- src/basic/ScheduleTable.tsx
+++ src/basic/ScheduleTable.tsx
@@ -18,6 +18,7 @@ import { fill2, parseHnM } from "./utils.ts";
 import { useDndContext, useDraggable } from "@dnd-kit/core";
 import { CSS } from '@dnd-kit/utilities';
 import { ComponentProps, Fragment } from "react";
+import ScheduleDndProvider from "./ScheduleDndProvider.tsx";
 
 interface Props {
   tableId: string;
@@ -59,71 +60,73 @@ const ScheduleTable = ({ tableId, schedules, onScheduleTimeClick, onDeleteButton
   const activeTableId = getActiveTableId();
 
   return (
-    <Box
-      position="relative"
-      outline={activeTableId === tableId ? "5px dashed" : undefined}
-      outlineColor="blue.300"
-    >
-      <Grid
-        templateColumns={`120px repeat(${DAY_LABELS.length}, ${CellSize.WIDTH}px)`}
-        templateRows={`40px repeat(${TIMES.length}, ${CellSize.HEIGHT}px)`}
-        bg="white"
-        fontSize="sm"
-        textAlign="center"
-        outline="1px solid"
-        outlineColor="gray.300"
+    <ScheduleDndProvider>
+      <Box
+        position="relative"
+        outline={activeTableId === tableId ? "5px dashed" : undefined}
+        outlineColor="blue.300"
       >
-        <GridItem key="교시" borderColor="gray.300" bg="gray.100">
-          <Flex justifyContent="center" alignItems="center" h="full" w="full">
-            <Text fontWeight="bold">교시</Text>
-          </Flex>
-        </GridItem>
-        {DAY_LABELS.map((day) => (
-          <GridItem key={day} borderLeft="1px" borderColor="gray.300" bg="gray.100">
-            <Flex justifyContent="center" alignItems="center" h="full">
-              <Text fontWeight="bold">{day}</Text>
+        <Grid
+          templateColumns={`120px repeat(${DAY_LABELS.length}, ${CellSize.WIDTH}px)`}
+          templateRows={`40px repeat(${TIMES.length}, ${CellSize.HEIGHT}px)`}
+          bg="white"
+          fontSize="sm"
+          textAlign="center"
+          outline="1px solid"
+          outlineColor="gray.300"
+        >
+          <GridItem key="교시" borderColor="gray.300" bg="gray.100">
+            <Flex justifyContent="center" alignItems="center" h="full" w="full">
+              <Text fontWeight="bold">교시</Text>
             </Flex>
           </GridItem>
-        ))}
-        {TIMES.map((time, timeIndex) => (
-          <Fragment key={`시간-${timeIndex + 1}`}>
-            <GridItem
-              borderTop="1px solid"
-              borderColor="gray.300"
-              bg={timeIndex > 17 ? 'gray.200' : 'gray.100'}
-            >
+          {DAY_LABELS.map((day) => (
+            <GridItem key={day} borderLeft="1px" borderColor="gray.300" bg="gray.100">
               <Flex justifyContent="center" alignItems="center" h="full">
-                <Text fontSize="xs">{fill2(timeIndex + 1)} ({time})</Text>
+                <Text fontWeight="bold">{day}</Text>
               </Flex>
             </GridItem>
-            {DAY_LABELS.map((day) => (
+          ))}
+          {TIMES.map((time, timeIndex) => (
+            <Fragment key={`시간-${timeIndex + 1}`}>
               <GridItem
-                key={`${day}-${timeIndex + 2}`}
-                borderWidth="1px 0 0 1px"
+                borderTop="1px solid"
                 borderColor="gray.300"
-                bg={timeIndex > 17 ? 'gray.100' : 'white'}
-                cursor="pointer"
-                _hover={{ bg: 'yellow.100' }}
-                onClick={() => onScheduleTimeClick?.({ day, time: timeIndex + 1 })}
-              />
-            ))}
-          </Fragment>
-        ))}
-      </Grid>
+                bg={timeIndex > 17 ? 'gray.200' : 'gray.100'}
+              >
+                <Flex justifyContent="center" alignItems="center" h="full">
+                  <Text fontSize="xs">{fill2(timeIndex + 1)} ({time})</Text>
+                </Flex>
+              </GridItem>
+              {DAY_LABELS.map((day) => (
+                <GridItem
+                  key={`${day}-${timeIndex + 2}`}
+                  borderWidth="1px 0 0 1px"
+                  borderColor="gray.300"
+                  bg={timeIndex > 17 ? 'gray.100' : 'white'}
+                  cursor="pointer"
+                  _hover={{ bg: 'yellow.100' }}
+                  onClick={() => onScheduleTimeClick?.({ day, time: timeIndex + 1 })}
+                />
+              ))}
+            </Fragment>
+          ))}
+        </Grid>
 
-      {schedules.map((schedule, index) => (
-        <DraggableSchedule
-          key={`${schedule.lecture.title}-${index}`}
-          id={`${tableId}:${index}`}
-          data={schedule}
-          bg={getColor(schedule.lecture.id)}
-          onDeleteButtonClick={() => onDeleteButtonClick?.({
-            day: schedule.day,
-            time: schedule.range[0],
-          })}
-        />
-      ))}
-    </Box>
+        {schedules.map((schedule, index) => (
+          <DraggableSchedule
+            key={`${schedule.lecture.title}-${index}`}
+            id={`${tableId}:${index}`}
+            data={schedule}
+            bg={getColor(schedule.lecture.id)}
+            onDeleteButtonClick={() => onDeleteButtonClick?.({
+              day: schedule.day,
+              time: schedule.range[0],
+            })}
+          />
+        ))}
+      </Box>
+    </ScheduleDndProvider>
   );
 };
 
