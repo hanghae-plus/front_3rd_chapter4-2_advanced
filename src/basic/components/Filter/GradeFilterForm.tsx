b14be6e2cef79486b8420920c33624ed14ab05db
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-29 03:24:08 +0900

    ♻️ refactor :: 학년을 선택하는 필터 컴포넌트로 분리

---
+++ src/basic/components/Filter/GradeFilterForm.tsx
@@ -0,0 +1,26 @@
+import { Checkbox, CheckboxGroup, FormControl, FormLabel, HStack } from '@chakra-ui/react';
+import { memo } from 'react';
+
+type Props = {
+  value?: number[];
+  onChange: (value: number[]) => void;
+};
+
+const GradeFilterForm = ({ value, onChange }: Props) => {
+  return (
+    <FormControl>
+      <FormLabel>학년</FormLabel>
+      <CheckboxGroup value={value} onChange={(value) => onChange(value.map(Number))}>
+        <HStack spacing={4}>
+          {[1, 2, 3, 4].map((grade) => (
+            <Checkbox key={grade} value={grade}>
+              {grade}학년
+            </Checkbox>
+          ))}
+        </HStack>
+      </CheckboxGroup>
+    </FormControl>
+  );
+};
+
+export default memo(GradeFilterForm);
