49507b46409f074643ccd180c628ac6ca3363855
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-29 03:51:57 +0900

    ♻️ refactor :: 스케줄의 전체적인 작업 제어를 담당하는 context로 기능 위임

--- src/basic/ScheduleContext.tsx
+++ src/basic/ScheduleContext.tsx
@@ -1,10 +1,24 @@
-import React, { createContext, PropsWithChildren, useContext, useState } from 'react';
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useState,
+  useCallback,
+  useMemo,
+} from 'react';
 import { Schedule } from './types.ts';
 import dummyScheduleMap from './dummyScheduleMap.ts';
+import { CellSize, DAY_LABELS } from './constants.ts';
+import { DragEndEvent } from '@dnd-kit/core';
 
 interface ScheduleContextType {
   schedulesMap: Record<string, Schedule[]>;
-  setSchedulesMap: React.Dispatch<React.SetStateAction<Record<string, Schedule[]>>>;
+  handleDuplicate: (targetId: string) => void;
+  handleRemove: (targetId: string) => void;
+  handleDelete: (targetId: string, day: string, time: number) => void;
+  handleAddSchedule: (targetId: string, schedules: Schedule[]) => void;
+  getSchedules: (tableId: string) => Schedule[];
+  updateSchedule: (event: DragEndEvent) => void;
 }
 
 const ScheduleContext = createContext<ScheduleContextType | undefined>(undefined);
@@ -20,9 +34,94 @@ export const useScheduleContext = () => {
 export const ScheduleProvider = ({ children }: PropsWithChildren) => {
   const [schedulesMap, setSchedulesMap] = useState<Record<string, Schedule[]>>(dummyScheduleMap);
 
-  return (
-    <ScheduleContext.Provider value={{ schedulesMap, setSchedulesMap }}>
-      {children}
-    </ScheduleContext.Provider>
+  const getSchedules = useCallback(
+    (tableId: string) => {
+      return schedulesMap[tableId] || [];
+    },
+    [schedulesMap]
   );
+
+  const updateSchedule = useCallback((event: DragEndEvent) => {
+    const { active, delta } = event;
+
+    const { x, y } = delta;
+    const [tableId, index] = (active.id as string).split(':');
+    const moveDayIndex = Math.floor(x / CellSize.WIDTH);
+    const moveTimeIndex = Math.floor(y / CellSize.HEIGHT);
+
+    setSchedulesMap((prev) => {
+      const schedule = prev[tableId][Number(index)];
+      const nowDayIndex = DAY_LABELS.indexOf(schedule.day as (typeof DAY_LABELS)[number]);
+
+      const updated = {
+        ...schedule,
+        day: DAY_LABELS[nowDayIndex + moveDayIndex],
+        range: schedule.range.map((time) => time + moveTimeIndex),
+      };
+
+      const updatedSchedules = prev[tableId].map((targetSchedule, targetIndex) => {
+        if (targetIndex !== Number(index)) {
+          return targetSchedule;
+        }
+        return updated;
+      });
+      return {
+        ...prev,
+        [tableId]: updatedSchedules,
+      };
+    });
+  }, []);
+
+  const handleDuplicate = useCallback((targetId: string) => {
+    setSchedulesMap((prev) => ({
+      ...prev,
+      [`schedule-${Date.now()}`]: [...prev[targetId]],
+    }));
+  }, []);
+
+  const handleRemove = useCallback((targetId: string) => {
+    setSchedulesMap((prev) => {
+      delete prev[targetId];
+      return { ...prev };
+    });
+  }, []);
+
+  const handleDelete = useCallback((targetId: string, day: string, time: number) => {
+    setSchedulesMap((prev) => ({
+      ...prev,
+      [targetId]: prev[targetId].filter(
+        (schedule) => schedule.day !== day || !schedule.range.includes(time)
+      ),
+    }));
+  }, []);
+
+  const handleAddSchedule = useCallback((targetId: string, schedules: Schedule[]) => {
+    setSchedulesMap((prev) => ({
+      ...prev,
+      [targetId]: [...prev[targetId], ...schedules],
+    }));
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      schedulesMap,
+      handleDuplicate,
+      handleRemove,
+      handleDelete,
+      handleAddSchedule,
+      getSchedules,
+      updateSchedule,
+    }),
+    [
+      schedulesMap,
+      handleDuplicate,
+      handleRemove,
+      handleDelete,
+      handleAddSchedule,
+      getSchedules,
+      updateSchedule,
+    ]
+  );
+
+  return <ScheduleContext.Provider value={value}>{children}</ScheduleContext.Provider>;
 };
