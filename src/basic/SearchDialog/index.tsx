c8f46b63640d693523831bd5e75670f832267af7
Hwang Tae Young <hty0525@naver.com>
2024-11-28 23:37:55 +0900

    SearchDialog 폴더로 분리

--- src/basic/SearchDialog/index.tsx
+++ src/basic/SearchDialog/index.tsx
@@ -29,11 +29,11 @@ import {
 	VStack,
 	Wrap,
 } from "@chakra-ui/react";
-import { useScheduleContext } from "./ScheduleContext.tsx";
-import { Lecture } from "./types.ts";
-import { parseSchedule } from "./utils.ts";
+import { useScheduleContext } from "../ScheduleContext.tsx";
+import { Lecture } from "../types.ts";
+import { parseSchedule } from "../utils.ts";
 import axios, { AxiosResponse } from "axios";
-import { DAY_LABELS } from "./constants.ts";
+import { DAY_LABELS } from "../constants.ts";
 
 interface Props {
 	searchInfo: {
